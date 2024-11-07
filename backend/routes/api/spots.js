605a6b14eda3f0f545d5e04a4a0c605e7727fb4e
Edforthewin <edwinramirezgomez14@gmail.com>
2024-11-07 16:57:27 -0500

    spot work

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Op, Sequelize } = require('sequelize');
+const { Op, Sequelize, ValidationError } = require('sequelize');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Spot, User } = require('../../db/models');
 const { check }= require('express-validator');
@@ -9,9 +9,46 @@ const { route } = require('./session.js')
 
 const router = express.Router();
 
+//CREATE SPOT
+router.post('/spots', requireAuth, async (req, res, next) => {
+    const ownerId = req.user.id;
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    const spot = await Spot.create({
+        ownerId,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    })
+    res.json(spot)
+});
+/////////////////////////////////////////////////////////////////////////////////////////
 
-router.get('/spots', async (req, res, next) => {
+//DELETE SPOT
+router.delete('/spots/:spotId', requireAuth, async (req, res, next) => {
+    const spotId = req.params.spotId;
+    const spot = req.params.spots;
+    await spot.destroy();
 
+     if(!spot) {
+        res.status(404).json({
+            "message": "Spot does not exist boi",
+            "statusCode": 404
+        })
+     } else {
+        res.json({
+            "message": "Successfully deleted",
+            "statusCode": 200
+        })
+     }
 })
+
+
+
 
 module.exports = router;
