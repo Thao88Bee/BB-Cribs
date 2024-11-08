2f8889c3e16f6d20dd8480be938c86601119e1ff
Thao88bee <beethao.bt@gmail.com>
2024-11-07 23:56:34 -0600

    testing

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-       Spot.belongTo(models.User, { foreignKey: 'ownerId' });
+       Spot.belongsTo(models.User, { foreignKey: 'ownerId' });
        Spot.hasMany(models.Booking, { foreignKey: 'spotId' });
     }
   }
