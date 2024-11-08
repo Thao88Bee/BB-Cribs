4b22449dcf57697acc747197a7dc1fd860837d9c
Thao88bee <beethao.bt@gmail.com>
2024-11-07 23:54:21 -0600

    testing

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -6,14 +6,8 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // define association here
-      User.hasMany(
-        models.Spot, {
-          foreignKey: 'ownerId',
-          onDelete: 'CASCADE',
-          hooks: true
-        }
-      );
-      User.hasMany(models.Booking, { foreignKey: 'userId', onDelete: 'CASCADE' })
+      User.hasMany(models.Spot, { foreignKey: 'ownerId', onDelete: 'CASCADE' });
+      User.hasMany(models.Booking, { foreignKey: 'userId', onDelete: 'CASCADE' });
     }
   }
 
