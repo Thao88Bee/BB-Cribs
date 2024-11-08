80e21e40e1ed81c95e3c99d3471747ffeb821ad1
Thao88bee <beethao.bt@gmail.com>
2024-11-08 12:20:45 -0600

    testing

--- backend/db/migrations/2-20241106211350-create-spot.js
+++ backend/db/migrations/2-20241106211350-create-spot.js
@@ -8,68 +8,72 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Spots", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      "Spots",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        ownerId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: "Users", key: "id" },
+          onDelete: "CASCADE",
+        },
+        address: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+        city: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        state: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        country: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        lat: {
+          type: Sequelize.DECIMAL,
+        },
+        lng: {
+          type: Sequelize.DECIMAL,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        description: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+        price: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        avgRating: {
+          type: Sequelize.DECIMAL,
+          allowNull: true,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: new Date(),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: new Date(),
+        },
       },
-      ownerId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: { model: "Users", key: "id" },
-        onDelete: "CASCADE",
-      },
-      address: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      city: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      state: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      country: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      lat: {
-        type: Sequelize.DECIMAL,
-      },
-      lng: {
-        type: Sequelize.DECIMAL,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      price: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      avgRating: {
-        type: Sequelize.DECIMAL,
-        allowNull: true,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: new Date(),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: new Date(),
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Spots";
--- backend/db/migrations/6-20241106002150-create-booking.js
+++ backend/db/migrations/6-20241106002150-create-booking.js
@@ -8,42 +8,46 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Bookings", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      "Bookings",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: "Spots", key: "id" },
+          onDelete: "CASCADE",
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: "Users", key: "id" },
+          onDelete: "CASCADE",
+        },
+        startDate: {
+          type: Sequelize.DATEONLY,
+          allowNull: false,
+        },
+        endDate: {
+          type: Sequelize.DATEONLY,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: true,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: true,
+          type: Sequelize.DATE,
+        },
       },
-      spotId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: { model: "Spots", key: "id" },
-        onDelete: "CASCADE",
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: { model: "Users", key: "id" },
-        onDelete: "CASCADE",
-      },
-      startDate: {
-        type: Sequelize.DATEONLY,
-        allowNull: false,
-      },
-      endDate: {
-        type: Sequelize.DATEONLY,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Bookings";
