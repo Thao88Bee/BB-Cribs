87259a038df002acc809cadead808385b81547f3
Thao88bee <beethao.bt@gmail.com>
2024-11-08 14:37:56 -0600

    testing

--- backend/db/seeders/3-20241108172050-demo-spot-images.js
+++ backend/db/seeders/3-20241108172050-demo-spot-images.js
@@ -8,13 +8,19 @@ if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
+const spotImages = [
+  
+]
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-
+    await Booking.bulkCreate(spotImages);
   },
 
   async down(queryInterface, Sequelize) {
-
+    options.tableName = "SpotImages";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, { [Op.or]: spotimages });
   },
 };
