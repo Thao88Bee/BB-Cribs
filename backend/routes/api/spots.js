57571fea09f09515981303e13802d575ecba5095
Edforthewin <edwinramirezgomez14@gmail.com>
2024-11-08 16:27:55 -0500

    Get details of a Spot from an id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { Op, Sequelize, ValidationError } = require("sequelize");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { Spot, User } = require("../../db/models");
+const { Spot, User, Review, SpotImage, Booking, ReviewImage, sequelize } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 const spot = require("../../db/models/spot.js");
@@ -12,12 +12,12 @@ const router = express.Router();
 const validateSpot = [
   check("address")
     .exists({ checkFalsy: true })
-    .withMessage("Street address is required"),
-  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
-  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
+    .withMessage("Street address is required boi"),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required boi"),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required boi"),
   check("country")
     .exists({ checkFalsy: true })
-    .withMessage("Country is required"),
+    .withMessage("Country is required dood"),
   check("lat").isDecimal().withMessage("Latitude is not valid"),
   check("lng").isDecimal().withMessage("Longitude is not valid"),
   check("name")
@@ -34,6 +34,48 @@ const validateSpot = [
   handleValidationErrors,
 ];
 
+
+
+//Get details of a Spot from an id
+router.get("/spots/:spotId", async (req, res, next) => {
+  const spotId = req.params.spotId;
+  const spot = await Spot.findByPk(spotId, {
+    include: [
+      {
+        model: Review,
+        attributes: []
+      },
+      {
+        model: SpotImage,
+        attributes: ["id", "url", "preview"]
+      },
+      {
+        model: User,
+        as: "Owner",
+        attributes: ["id", "firstName", "lastName"]
+      }
+    ],
+     attributes: {
+      include: [
+        [
+          sequelize.fn("ROUND", sequelize.fn("AVG", sequelize.col("Reviews.stars")), 2), "avgStarRating"
+        ],
+      ]
+     },
+     group: ["Owner.id", "Reviews.id", "SpotImages.id", "Spot.id"],
+  });
+   if(spot) {
+    res.json(spot);
+   } else{
+    res.status(404)
+    .json({
+      "message": "Spot couldn't be found"
+    })
+   }
+});
+
+
+////////////////////////////////////////////////////////////////////////////
 //Create a Spot
 router.post("/", requireAuth, validateSpot, async (req, res, next) => {
   const ownerId = req.user.id;
