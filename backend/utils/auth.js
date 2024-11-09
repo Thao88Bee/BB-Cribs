c881ad78976f1600ca775d73c3a87bdac0604132
Edforthewin <edwinramirezgomez14@gmail.com>
2024-11-08 19:11:25 -0500

    small refactor code

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
-const { User } = require('../db/models');
+const { User, Spot, Review, ReviewImage, SpotImage, Booking } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
 
@@ -17,9 +17,9 @@ const setTokenCookie = (res, user) => {
         secret,
         { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
     );
-  
+
     const isProduction = process.env.NODE_ENV === "production";
-  
+
     // Set the token cookie
     res.cookie('token', token, {
         maxAge: expiresIn * 1000, // maxAge in milliseconds
@@ -27,7 +27,7 @@ const setTokenCookie = (res, user) => {
         secure: isProduction,
         sameSite: isProduction && "Lax"
     });
-  
+
     return token;
 };
 
@@ -35,12 +35,12 @@ const restoreUser = (req, res, next) => {
     // token parsed from cookies
     const { token } = req.cookies;
     req.user = null;
-  
+
     return jwt.verify(token, secret, null, async (err, jwtPayload) => {
         if (err) {
             return next();
         }
-  
+
         try {
             const { id } = jwtPayload.data;
             req.user = await User.findByPk(id, {
@@ -52,9 +52,9 @@ const restoreUser = (req, res, next) => {
             res.clearCookie('token');
             return next();
         }
-  
+
         if (!req.user) res.clearCookie('token');
-  
+
         return next();
     });
 };
@@ -62,7 +62,7 @@ const restoreUser = (req, res, next) => {
 // If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
     if (req.user) return next();
-  
+
     const err = new Error('Authentication required');
     err.title = 'Authentication required';
     err.errors = { message: 'Authentication required' };
