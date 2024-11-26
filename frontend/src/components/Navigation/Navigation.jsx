ace0f14ff6d42cb15edeb77965b48ac5c287db81
Thao88bee <beethao.bt@gmail.com>
2024-11-26 14:21:03 -0600

    SomeCss

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -2,14 +2,18 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
+import bblogo from "../../../public/favicon.ico"
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
   return (
     <ul id="navBar">
+      <Link to="/">
+      <img src={bblogo} alt="BB Cribs Logo" />
+      </Link>
       <li>
-        <Link className="Link" to="/">Home</Link>
+        <Link className="Link" to="/">Spots</Link>
       </li>
       {isLoaded && (
         <li>
