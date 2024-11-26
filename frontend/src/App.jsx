17454ffef0913411b9f66f6423242283e2ec9f5c
Thao88bee <beethao.bt@gmail.com>
2024-11-25 22:11:41 -0600

    LoginModal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage/SignupFormPage";
-import Navigation from "./components/Navigation/Navigation";
-import * as sessionActions from "./store/session";
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import SignupFormPage from './components/SignupFormPage/SignupFormPage';
+import Navigation from './components/Navigation/Navigation';
+import * as sessionActions from './store/session';
 
 function Layout() {
   const dispatch = useDispatch();
@@ -12,7 +11,7 @@ function Layout() {
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => {
-      setIsLoaded(true);
+      setIsLoaded(true)
     });
   }, [dispatch]);
 
@@ -29,19 +28,15 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/",
-        element: <h1>Welcome!</h1>,
-      },
-      {
-        path: "login",
-        element: <LoginFormPage />,
+        path: '/',
+        element: <h1>Welcome!</h1>
       },
       {
         path: "signup",
-        element: <SignupFormPage />,
-      },
-    ],
-  },
+        element: <SignupFormPage />
+      }
+    ]
+  }
 ]);
 
 function App() {
