85856b7f3bf1b9c24e85f145bf6bd8b0164f092e
Thao88bee <beethao.bt@gmail.com>
2024-12-03 16:05:38 -0600

    ProfileBtn

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -63,10 +63,7 @@ function ProfileButton({ user }) {
       <ul className={ulClassName} ref={ulRef}>
         {user ? (
           <>
-            <li>{user.username}</li>
-            <li>
-              {user.firstName} {user.lastName}
-            </li>
+            <li>Hello, {user.firstName}</li>
             <li>{user.email}</li>
             <li>
               <br />
