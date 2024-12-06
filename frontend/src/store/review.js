2cafba547b95f4b3b12455f9b387925176de537f
Thao88bee <beethao.bt@gmail.com>
2024-12-06 06:59:27 -0600

    Reviews

--- frontend/src/store/review.js
+++ frontend/src/store/review.js
@@ -4,6 +4,7 @@ import { csrfFetch } from "./csrf";
 
 const GET_SPOT_REVIEWS = "review/getSpotReviews";
 const CREATE_REVIEW = "review/createReview";
+const UPDATE_REVIEW = "review/updateReview";
 const DELETE_REVIEW = "review/deleteReview";
 
 export const getSpotReviewsAction = (spot) => {
@@ -17,6 +18,13 @@ export const createReviewAction = (spot) => {
   return {
     type: CREATE_REVIEW,
     spot,
+  };
+};
+
+export const updateReviewAction = (reviewId) => {
+  return {
+    type: UPDATE_REVIEW,
+    reviewId,
   };
 };
 
@@ -47,7 +55,23 @@ export const createReview = (newReview, spotId) => async (dispatch) => {
   if (res.ok) {
     const data = await res.json();
     dispatch(createReviewAction(data));
-    return res;
+    return data;
+  }
+};
+
+export const updateReview = (updatedReview, reviewId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/reviews/${reviewId}`, {
+    method: "PUT",
+    headers: {
+      "Context-Type": "application/json",
+    },
+    body: JSON.stringify(updatedReview),
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(updateReviewAction(data));
+    return data;
   }
 };
 
@@ -59,6 +83,8 @@ const reviewsReducer = (state = initialState, action) => {
       return { ...state, Reviews: action.spot.Reviews };
     case CREATE_REVIEW:
       return { ...state, Reviews: action.reviews };
+    case UPDATE_REVIEW:
+      return { ...state, Reviews: action.reviews }
     case DELETE_REVIEW:
       return { ...state, Reviews: action.reviews };
     default:
